3ec0feeba2f0bfdbe24bc9558e3862adb9c5dbd6
shkang2 <shkang2@piegroup.co.kr>
2023-11-22 00:18:09 +0900

    Async Accept 구현 방식 기록용 커밋

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -6,8 +9,40 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            while (true) { }
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            while (true)
+            {
+                // 소켓 설정
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"Connected To {socket.RemoteEndPoint?.ToString()}");
+
+                    // Send
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                    int sendBytes = socket.Send(sendBuff);
+
+                    // Recv
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Thread.Sleep(100);
+            }
         }
     }
 }
---
+++ ServerCore/Listener.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    internal class Listener
+    {
+        Socket _listenerSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public Listener(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _onAcceptHandler += onAcceptHandler;
+            _listenerSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind
+            _listenerSocket.Bind(endPoint);
+
+            // 최대 대기수(10)
+            _listenerSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            bool pending = _listenerSocket.AcceptAsync(args);
+            if (pending == false)
+            {
+                OnAcceptCompleted(null, args);
+            }
+        }
+
+        void OnAcceptCompleted(object? sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success)
+            {
+                // TODO : 
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            RegisterAccept(args);
+        }
+
+        public Socket Accept()
+        {
+            _listenerSocket.AcceptAsync();
+            return _listenerSocket.Accept();
+        }
+    }
+}

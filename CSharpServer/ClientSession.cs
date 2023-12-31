38e2794882d0acf2eabf0d409daa6609abdf3d88
shkang2 <shkang2@piegroup.co.kr>
2024-01-01 01:50:45 +0900

    Serialization #2 기록용 커밋

---
+++ CSharpServer/ClientSession.cs
@@ -0,0 +1,134 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public abstract class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+
+        public abstract void Read(ArraySegment<byte> s);
+        public abstract ArraySegment<byte> Write();
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+        public string name;
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> segment)
+        {
+            ushort count = 0;
+
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+            // ushort size = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            this.playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.packetId);
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
+            count += sizeof(long);
+            success &= BitConverter.TryWriteBytes(s, count);
+
+            if (success == false)
+            {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);
+        }
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,
+    }
+
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            // Packet packet = new Packet() { size = 100, packetId = 10 };
+
+            // ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            // 
+            // byte[] buffer = BitConverter.GetBytes(packet.size);
+            // byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+            // Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            // Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            // 
+            // ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            // base.Send(sendBuff);
+
+            Thread.Sleep(5000);
+            base.Disconnect();
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            switch((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:
+                    {
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+
+                        Console.WriteLine($"PlayerInfoReq : {p.playerId}");
+                    }
+                    break;
+
+                case PacketID.PlayerInfoOk:
+                    break;
+            }
+
+            Console.WriteLine($"Recv PacketID : {id}, Recv size : {size}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+    }
+}
---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,110 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    public abstract class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+
+        public abstract void Read(ArraySegment<byte> s);
+        public abstract ArraySegment<byte> Write();
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+        public string name;
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> segment)
+        {
+            ushort count = 0;
+
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+            // ushort size = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            this.playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count , s.Length - count), this.packetId);
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
+            count += sizeof(long);
+            success &= BitConverter.TryWriteBytes(s, count);
+
+            if(success == false)
+            {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);
+        }
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,
+    }
+
+    class ServerSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
+            
+            // Send
+            // for (int i = 0; i < 5; i++)
+            {
+                ArraySegment<byte> s = packet.Write();
+                if(s != null)
+                {
+                    base.Send(s);
+                }
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
